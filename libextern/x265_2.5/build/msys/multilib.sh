fc0c7550a90dfc64ad6f722ec64efd77ee329a5b
jfiguinha <jfiguinha@hotmail.fr>
2019-06-21 15:19:39 +0200

    update file

--- libextern/x265_2.5/build/msys/multilib.sh
+++ libextern/x265_2.5/build/msys/multilib.sh
@@ -6,17 +6,17 @@ mkdir -p 8bit 10bit 12bit
 
 cd 12bit
 cmake -G "MSYS Makefiles" ../../../source -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DMAIN12=ON
-make ${MAKEFLAGS} -j $NBPROC
+make ${MAKEFLAGS} -j$NBPROC
 cp libx265.a ../8bit/libx265_main12.a
 
 cd ../10bit
 cmake -G "MSYS Makefiles" ../../../source -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF
-make ${MAKEFLAGS} -j $NBPROC
+make ${MAKEFLAGS} -j$NBPROC
 cp libx265.a ../8bit/libx265_main10.a
 
 cd ../8bit
 cmake -G "MSYS Makefiles" ../../../source -DEXTRA_LIB="x265_main10.a;x265_main12.a" -DEXTRA_LINK_FLAGS=-L. -DLINKED_10BIT=ON -DLINKED_12BIT=ON
-make ${MAKEFLAGS} -j $NBPROC
+make ${MAKEFLAGS} -j$NBPROC
 
 # rename the 8bit library, then combine all three into libx265.a using GNU ar
 mv libx265.a libx265_main.a
