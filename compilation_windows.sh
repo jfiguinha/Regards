29534725fb872067e06e158e73b725761a60d73d
jfiguinha <jfiguinha@hotmail.fr>
2020-01-17 10:46:05 +0100

    Update compilation bash windows

--- compilation_windows.sh
+++ compilation_windows.sh
@@ -23,6 +23,7 @@ pacman -S mingw-w64-x86_64-libexif
 pacman -S mingw-w64-x86_64-gnutls
 pacman -S mingw-w64-x86_64-leptonica
 pacman -S mingw-w64-x86_64-icu
+pacman -S mingw-w64-x86_64-intel-tbb
 #install OpenCL SDK
 #wget https://github.com/GPUOpen-LibrariesAndSDKs/OCL-SDK/releases/download/1.0/OCL_SDK_Light_AMD.exe
 
