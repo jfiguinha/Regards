3d8a8d14e89cb2728b3880ce0e000c85a3828eb3
Figuinha <jfiguinha@hotmail.fr>
2020-02-09 18:28:56 +0100

    Update project for av1

--- libextern/ffmpeg_build_windows.sh
+++ libextern/ffmpeg_build_windows.sh
@@ -2,8 +2,18 @@
 NBPROC=$(nproc)
 echo $NBPROC
 
-# libaom
 tar xf ffmpeg-4.2.2.tar.xz
+
+# libaom
+git clone https://aomedia.googlesource.com/aom
+cd aom
+mkdir build
+cd build
+cmake ../../aom -G"MSYS Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="$HOME/ffmpeg_build"
+make -j$NBPROC
+make install
+cd ..
+cd ..
 
 export PKG_CONFIG_PATH=$HOME/ffmpeg_build/lib/pkgconfig
 
@@ -11,7 +21,7 @@ export PKG_CONFIG_PATH=$HOME/ffmpeg_build/lib/pkgconfig
 #git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg-4.2.2
 ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" \
-  --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="/usr/bin" --enable-gpl --enable-nonfree
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="/usr/bin" --enable-gpl --enable-nonfree --enable-libaom
 make -j$NBPROC
 #make install
 cd ..
