ea29fc1df67ac14eaa1c1f1ef7a923d4b2db2fc4
jfiguinha <jfiguinha@icloud.com>
2020-06-01 19:52:22 +0200

    Update project macos

--- compilation_macos.sh
+++ compilation_macos.sh
@@ -6,21 +6,25 @@ echo $NBPROC
 brew update 
 brew upgrade
 xcode-select --install
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v pkgconfig
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v llvm@7
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v pango 
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v libexif 
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v OpenEXR
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v glew
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v coreutils
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v leptonica
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v libjpeg-turbo
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v wget
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v unzip
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v cmake
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v yasm
-HOMEBREW_MAKE_JOBS=$NBPROC brew install -v tbb
-#bHOMEBREW_MAKE_JOBS=$NBPROC rew install -v fftw
+brew install -v pkgconfig
+brew install -v llvm@7
+brew install -v pango 
+brew install -v libexif 
+brew install -v OpenEXR
+brew install -v glew
+brew install -v coreutils
+brew install -v leptonica
+brew install -v libjpeg-turbo
+brew install -v wget
+brew install -v unzip
+brew install -v cmake
+brew install -v yasm
+brew install -v tbb
+brew install -v libtiff
+brew install -v libpng
+brew install -v libjpeg
+brew install -v libfreeimage
+#brew install -v fftw
 #Install External Library
 cd libextern
 chmod +x compilation_library_macos.sh
