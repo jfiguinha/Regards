1ac72cd0bc097b73d9522175422744190481a923
jfiguinha <jfiguinha@hotmail.fr>
2020-06-28 12:20:53 +0200

    Update compilation file for linux

--- compilation.sh
+++ compilation.sh
@@ -1,1 +1,10 @@
-/bin/sh -c '/usr/bin/make -j12 -e -f Makefile'
+#!/bin/bash
+NBPROC=$(nproc)
+echo $NBPROC
+
+unzip project_linux.zip
+
+sudo cp libextern/tesseract-4.1.0/tesscallback.h ~/ffmpeg_build/include/tesseract
+
+export PROJECTPATH=$PWD
+make -j$NBPROC
