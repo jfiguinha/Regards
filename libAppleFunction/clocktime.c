0152f1e588f7b255942cf1bbb74a4ffcad849006
jfiguinha <jfiguinha@hotmail.fr>
2020-08-08 11:50:35 +0200

    Update apple project

--- libAppleFunction/clocktime.c
+++ libAppleFunction/clocktime.c
@@ -1,22 +1,24 @@
-#include <mach/mach_time.h>
-#define ORWL_NANO (+1.0E-9)
-#define ORWL_GIGA UINT64_C(1000000000)
+#include <time.h>
+#include <sys/time.h>
 
-static double orwl_timebase = 0.0;
-static uint64_t orwl_timestart = 0;
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
 
-struct timespec orwl_gettime(void) {
-  // be more careful in a multithreaded environement
-  if (!orwl_timestart) {
-    mach_timebase_info_data_t tb = { 0 };
-    mach_timebase_info(&tb);
-    orwl_timebase = tb.numer;
-    orwl_timebase /= tb.denom;
-    orwl_timestart = mach_absolute_time();
-  }
-  struct timespec t;
-  double diff = (mach_absolute_time() - orwl_timestart) * orwl_timebase;
-  t.tv_sec = diff * ORWL_NANO;
-  t.tv_nsec = diff - (t.tv_sec * ORWL_GIGA);
-  return t;
-}
+
+
+struct timespec ts;
+
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+clock_serv_t cclock;
+mach_timespec_t mts;
+host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+clock_get_time(cclock, &mts);
+mach_port_deallocate(mach_task_self(), cclock);
+ts.tv_sec = mts.tv_sec;
+ts.tv_nsec = mts.tv_nsec;
+
+#else
+clock_gettime(CLOCK_REALTIME, &ts);
+#endif
