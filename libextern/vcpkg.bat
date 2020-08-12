0ef42ccc441c7131b6df64d7322c8716ec677797
Jacques Figuinha <jfiguinha@hotmail.fr>
2020-08-12 17:49:30 +0200

    Update visual studio project

---
+++ libextern/vcpkg.bat
@@ -0,0 +1,7 @@
+vcpkg install cairo:x64-windows --recurse
+vcpkg install libxml2:x64-windows --recurse
+vcpkg install libexif:x64-windows --recurse
+vcpkg install ffmpeg:x64-windows --recurse
+vcpkg install freeimage:x64-windows --recurse
+vcpkg install tesseract:x64-windows --recurse
+vcpkg install tbb:x64-windows --recurse
