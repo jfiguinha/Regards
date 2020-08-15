9c97567b757f5b40cfe9f5153d1d9a21bcf8c3ce
Figuinha <jfiguinha@hotmail.fr>
2020-08-15 17:18:07 +0200

    Update library compilation linux

--- libextern/compilation_library_linux.sh
+++ libextern/compilation_library_linux.sh
@@ -14,16 +14,6 @@ tar xf poppler-0.81.0.tar.xz
 wget https://github.com/tesseract-ocr/tesseract/archive/4.1.1.zip
 unzip 4.1.1.zip
 
-#install leptonica
-tar xf  leptonica-1.79.0.tar.gz
-cd leptonica-1.79.0
-mkdir build
-cd build
-cmake ../  -DCMAKE_INSTALL_PREFIX:PATH="$HOME/ffmpeg_build"
-make -j$NBPROC
-sudo make install
-cd ..
-cd ..
 
 #compile tesseract
 cd tesseract-4.1.1
@@ -140,7 +130,7 @@ unzip opencv_contrib-4.3.0.zip
 cd opencv-4.3.0
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX:PATH="$HOME/ffmpeg_build" -DBUILD_opencv_python=OFF -DOPENCV_EXTRA_MODULES_PATH:PATH="../../opencv_contrib-4.3.0/modules" -DBUILD_opencv_freetype=OFF -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_apps=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_CXX_FLAGS="-std=gnu++14 -I ../../../eigen/Eigen" -DOPENCV_ALLOCATOR_STATS_COUNTER_TYPE=int64_t ../
+cmake -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DCMAKE_INSTALL_PREFIX:PATH="$HOME/ffmpeg_build" -DBUILD_opencv_python=OFF -DOPENCV_EXTRA_MODULES_PATH:PATH="../../opencv_contrib-4.3.0/modules" -DBUILD_opencv_freetype=OFF -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_apps=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_CXX_FLAGS="-std=gnu++14 -I ../../../eigen/Eigen" -DOPENCV_ALLOCATOR_STATS_COUNTER_TYPE=int64_t ../
 make -j$NBPROC
 sudo make install
 cd ..
