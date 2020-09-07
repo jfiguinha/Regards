cf18655147d5ef30db7b310cba0064434277d994
Jacques Figuinha <jfiguinha@hotmail.fr>
2020-09-07 22:33:36 +0200

    Update saving heic

--- libPicture/avif.cpp
+++ libPicture/avif.cpp
@@ -218,6 +218,7 @@ void CAvif::SavePicture(const string &filenameOut, const int & type, CRegardsBit
 			uint8_t* p = heif_image_get_plane(image, heif_channel_interleaved, &stride);
 			uint8_t * data = source->GetPtBitmap();
 			source->ConvertToBgr();
+			source->HorzFlipBuf();
 			for (uint32_t y = 0; y < source->GetBitmapHeight(); y++)
 			{
 				int position = source->GetPosition(0, y);
