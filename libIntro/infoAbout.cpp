91a4dabe42fd105e6a629f4b545b246f1a90bbc8
Jacques Figuinha <jfiguinha@hotmail.fr>
2020-09-22 15:14:28 +0200

    Update version to 2.61.9

--- libIntro/infoAbout.cpp
+++ libIntro/infoAbout.cpp
@@ -60,7 +60,7 @@ void CInfoAbout::Init()
 	CTreeData * treeDataWidth = new CTreeData();
 	treeDataWidth->SetIsParent(false);
 	treeDataWidth->SetKey("Version");
-	treeDataWidth->SetValue("2.61.8");
+	treeDataWidth->SetValue("2.61.9");
 	tr.append_child(child, treeDataWidth);
 
 	CTreeData * treeDataAnnee = new CTreeData();
