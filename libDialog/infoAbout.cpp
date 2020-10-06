94f64d2b3656f395735b482dc7d29f56a37428dd
Jacques Figuinha <jfiguinha@hotmail.fr>
2020-10-06 17:16:09 +0200

    Update version to 2.62.3

--- libDialog/infoAbout.cpp
+++ libDialog/infoAbout.cpp
@@ -60,7 +60,7 @@ void CInfoAbout::Init()
 	CTreeData * treeDataWidth = new CTreeData();
 	treeDataWidth->SetIsParent(false);
 	treeDataWidth->SetKey("Version");
-	treeDataWidth->SetValue("2.62.2");
+	treeDataWidth->SetValue("2.62.3");
 	tr.append_child(child, treeDataWidth);
 
 	CTreeData * treeDataAnnee = new CTreeData();
