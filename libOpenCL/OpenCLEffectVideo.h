c5cc81f257bbec2bb9ed8800fe60d9c80df2b32c
Jacques Figuinha <jfiguinha@hotmail.fr>
2020-11-16 23:11:18 +0100

    Update shader

--- libOpenCL/OpenCLEffectVideo.h
+++ libOpenCL/OpenCLEffectVideo.h
@@ -38,10 +38,7 @@ namespace Regards
 			virtual void InterpolationBicubic(const int& widthOut, const int& heightOut, const int &flipH, const int &flipV, const int& angle, const int& bicubic);
 			virtual void InterpolationZoomBicubic(const int& widthOutput, const int& heightOutput, const wxRect &rc, const int &flipH, const int &flipV, const int& angle, const int& bicubic);
 			virtual void TranscodePicture(const int &widthOut, const int &heightOut) {};
-			int SetAlphaValue(cl_mem cl_image, const int& width, const int& height, const int& alphaValue);
 			void ApplyVideoEffect(CVideoEffectParameter * videoEffectParameter);
-			//int InterpolationBicubicOpenGLTexture(cl_mem cl_image, const int& width, const int& height, const int& widthOutput, const int& heightOutput, const int & flipH, const int &flipV, const int& angle, const int& bicubic);
-			//int InterpolationBicubicZoneOpenGLTexture(cl_mem cl_openglTexture, const int& width, const int& height, const int& widthOutput, const int& heightOutput, const wxRect &rc, const int & flipH, const int &flipV, const int& angle, const int& bicubic);
             void FlipVertical();
 			void GetRgbaBitmap(cl_mem cl_image, int rgba = 0);
 			int GetWidth();
