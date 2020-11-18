c9729d1af2df1ba056884ea32cf8a625045b3b0f
figuinha <jfiguinha@hotmail.fr>
2020-11-18 10:49:48 +0100

    add missing file for mac os x

--- libextern/ffmpeg_build_macos.sh
+++ libextern/ffmpeg_build_macos.sh
@@ -19,7 +19,8 @@ export PKG_CONFIG_PATH=$HOME/ffmpeg_build/lib/pkgconfig
 
 # ffmpeg
 #git clone --depth 1 git://source.ffmpeg.org/ffmpeg
-cd ffmpeg-4.3.1
+mv ffmpeg-4.3.1 ffmpeg-master
+cd ffmpeg-master
 ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" \
   --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="/usr/bin" --enable-gpl --enable-nonfree --enable-libaom
 make -j$NBPROC
