81e1080bbe76eff83ed73e4eb10cb6c46a3474f1
figuinha <jfiguinha@hotmail.fr>
2020-11-26 14:41:35 +0100

    Update code

--- compilation.sh
+++ compilation.sh
@@ -4,7 +4,7 @@ echo $NBPROC
 
 unzip project_linux.zip
 
-sudo cp libextern/tesseract-4.1.0/tesscallback.h ~/ffmpeg_build/include/tesseract
+sudo cp libextern/tesscallback.h ~/ffmpeg_build/include/tesseract
 
 export PROJECTPATH=$PWD
 make -j$NBPROC
