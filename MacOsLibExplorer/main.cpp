4dc2f15e312196c26340576c3aeaefd4e9c514f0
figuinha <jfiguinha@hotmail.fr>
2020-12-09 22:05:39 +0100

    Update mac project lib

--- MacOsLibExplorer/main.cpp
+++ MacOsLibExplorer/main.cpp
@@ -22,21 +22,34 @@ int main( int argc, char** argv )
 	// initialize wxWidgets
 	wxInitializer init;
     wxArrayString files;
-    wxTextFile txtFile(argv[1]); 
+    wxTextFile txtFile(argv[2]); 
     txtFile.Create(); 
     
 	wxPrintf( wxT("Hello in wxWidgets World!\n\n") );
-    wxString dirPath = argv[0];
-    printf("directory %s \n",dirPath.ToUTF8());
+    wxString dirPath = argv[1];
+    printf("directory %s \n",dirPath.ToStdString().c_str());
     wxString filename;
     size_t nbFiles =  wxDir::GetAllFiles(dirPath, &files);
     for(int i = 0;i < nbFiles;i++)
     {
         wxArrayString output;
-        printf("%s\n", files[i]);
+        printf("File : %s\n", files[i].ToStdString().c_str());
         wxString command = "otool -L " + files[i];
         wxExecute(command.c_str(), output);
-        SaveArrayStringToFile(txtFile, output);
+        //SaveArrayStringToFile(txtFile, output);
+        for(int i=2;i<output.GetCount();i++) 
+        { 
+            wxString lineText = output.Item(i);
+            //int index = lineText.find("/System/Library/Frameworks");
+             //printf("index %i \n", index);
+            
+            if(!(lineText.find("/System/Library/Frameworks") == 1 || lineText.find("/usr/lib") == 1 || lineText.find("@executable_path") == 1))
+            {
+                printf("Line %s\n", lineText.ToStdString().c_str());
+                txtFile.AddLine(lineText);                
+            }
+
+        } 
     }
     
     txtFile.Close(); 
