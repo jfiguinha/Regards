91480fb8c24ad93c9ed6408378b180dce3838505
figuinha jacques <jacques@MacBook-Air-de-figuinha.home>
2020-12-30 16:30:54 +0100

    Update project for apple silicon

--- libextern/compilation_library_macos.sh
+++ libextern/compilation_library_macos.sh
@@ -49,21 +49,13 @@ cd ..
 cd ..
 cd ..
 
-#compile lame
-tar xf lame-3.100.tar.gz
-cd lame-3.100
-./configure --prefix=/Users/jacques/ffmpeg_build --enable-static --disable-shared
-make -j$NBPROC
-sudo make install
-cd ..
-
 export PKG_CONFIG_PATH=$HOME/ffmpeg_build/lib/pkgconfig
 
 # ffmpeg
 tar xf ffmpeg-4.3.1.tar.xz
 mv ffmpeg-4.3.1 ffmpeg-master
 cd ffmpeg-master
-./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/ffmpeg_build/bin" --enable-gpl --enable-nonfree --enable-libaom --enable-libfdk-aac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-libopus --enable-libxvid --enable-libdav1d --enable-shared
+./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include -I/opt/homebrew/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib -L/opt/homebrew/lib" --bindir="$HOME/ffmpeg_build/bin" --enable-gpl --enable-nonfree --enable-libaom --enable-libfdk-aac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-libopus --enable-libxvid --enable-libdav1d --enable-shared
 make -j$NBPROC
 sudo make install
 
