ed5c872128e55f413fe86e4e2c3fde10b4e06c88
Figuinha Jacques <jfiguinha@hotmail.fr>
2020-12-31 14:18:26 +0100

    Delete build folder

--- libextern/libde265-master/build/libde265/x86/CMakeFiles/x86_sse.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/x86_sse.dir/sse-dct.cc.obj"
-  "CMakeFiles/x86_sse.dir/sse-motion.cc.obj"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/x86_sse.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
