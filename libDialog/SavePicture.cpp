e955976206f77135ff19e699a235869491bf927e
Jacques Figuinha <jfiguinha@hotmail.fr>
2021-01-10 09:55:09 +0100

    Update save dialog on osx

--- libDialog/SavePicture.cpp
+++ libDialog/SavePicture.cpp
@@ -29,122 +29,8 @@ wxString CSavePicture::SelectExternalFormat(wxWindow * window, const wxString &f
 
 
 #ifdef __APPLE__
-
-
-		SaveFileFormat saveFormat(nullptr);
-		saveFormat.ShowModal();
-		if (!saveFormat.IsOk())
-			return "";
-
-		wxString extension;
-		int iFormat = saveFormat.GetSaveFormat();
-		switch (iFormat)
-		{
-		case 0:
-			iFormat = BMP;
-			extension = "bmp";
-			break;
-
-		case 1:
-			iFormat = TGA;
-			extension = "tga";
-			break;
-
-		case 2:
-			iFormat = PCX;
-			extension = "pcx";
-			break;
-
-		case 3:
-			iFormat = MNG;
-			extension = "mng";
-			break;
-
-		case 4:
-			iFormat = TIFF;
-			extension = "tif";
-			break;
-
-		case 5:
-			iFormat = PNG;
-			extension = "png";
-			break;
-
-		case 6:
-			iFormat = GIF;
-			extension = "gif";
-			break;
-
-		case 7:
-			iFormat = JPEG;
-			extension = "jpg";
-			break;
-
-		case 8:
-			iFormat = PNM;
-			extension = "pnm";
-			break;
-
-		case 9:
-			iFormat = JPC;
-			extension = "jpc";
-			break;
-
-		case 10:
-			iFormat = JPEG2000;
-			extension = "jp2";
-			break;
-
-		case 11:
-			iFormat = PPM;
-			extension = "ppm";
-			break;
-
-		case 12:
-			iFormat = XPM;
-			extension = "xpm";
-			break;
-
-		case 13:
-			iFormat = WEBP;
-			extension = "webp";
-			break;
-
-		case 14:
-			iFormat = PDF;
-			extension = "pdf";
-			break;
-
-		case 15:
-			iFormat = JXR;
-			extension = "jxr";
-			break;
-
-		case 16:
-			iFormat = IFF;
-			extension = "iff";
-			break;
-
-		case 17:
-			iFormat = EXR;
-			extension = "exr";
-			break;
-
-		case 18:
-			iFormat = BPG;
-			extension = "bpg";
-			break;
-
-		case 19:
-			iFormat = PFM;
-			extension = "pfm";
-			break;
-		}
-
-		file = saveFileProgrammaticVersion(CConvertUtility::ConvertToStdString(extension));
-
-
-#else
+		wxSystemOptions::SetOption(wxOSX_FILEDIALOG_ALWAYS_SHOW_TYPES, 1);
+#endif
 
 		std::vector<wxString> v = { ".pdf",".pnm",".bmp",".bpg",".pcx",".jpg",".tif",".gif",".png",".tga",".jp2",".jpc",".ppm",".mng",".webp",".iff",".xpm",".jxr",".exr",".j2k",".pfm",".avif",".heic" };
 		szFilter = "Files PDF(*.PDF) | *.pdf|Files PNM (*.PNM)|*.pnm|Files BMP(*.BMP)|*.bmp|Files BPG(*.BPG)|*.bpg|Files PCX(*.PCX)|*.pcx|Files JPEG(*.JPG)|*.jpg|Files TIFF(*.TIF)|*.tif|Files GIF(*.GIF)|*.gif| Files PNG(*.PNG)|*.png|Files TGA(*.TGA)|*.tga|Files JPEG2000(*.JP2)|*.jp2|Files JPC(*.JPC)|*.jpc|Files PPM(*.PPM)|*.ppm|Files MNG(*.MNG)|*.mng|Files WEBP (*.WEBP)|*.webp|Files IFF (*.IFF)|*.iff|Files XPM (*.XPM)|*.xpm|Files JXR (*.JXR)|*.jxr|Files EXR (*.EXR)|*.exr|Files J2K (*.J2K)|*.j2k|Files PFM (*.PFM)|*.pfm|Files AVIF (*.avif)|*.avif|Files HEIC (*.heic)|*.heic";
@@ -167,7 +53,6 @@ wxString CSavePicture::SelectExternalFormat(wxWindow * window, const wxString &f
 		if (extension != selectedExtension)
 			file.append(selectedExtension);
 
-#endif
 	}
 	return file;
 
