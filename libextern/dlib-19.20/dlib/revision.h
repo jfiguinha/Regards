7d93b58eac1f47e9be22506377909c075a365d3c
Jacques Figuinha <jfiguinha@hotmail.fr>
2021-03-27 13:38:48 +0100

    Update project with dlib19.21

--- libextern/dlib-19.20/dlib/revision.h
+++
@@ -1,8 +0,0 @@
-#ifndef DLIB_REVISION_H
-// Version:  19.20
-// Date:     Sat Jun 6 14:53:53 EDT 2020
-// Git Changeset ID:  5612caa16937b5b81eb2dba17c20e303537a6a9d
-#define DLIB_MAJOR_VERSION  19
-#define DLIB_MINOR_VERSION  20
-#define DLIB_PATCH_VERSION  0
-#endif
