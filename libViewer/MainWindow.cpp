2a4af6fc2e6f215c56e5c04503f5b3e5953795c5
Jacques Figuinha <jfiguinha@hotmail.fr>
2021-05-22 16:14:54 +0200

    Add last file open

--- libViewer/MainWindow.cpp
+++ libViewer/MainWindow.cpp
@@ -999,6 +999,10 @@ CMainWindow::~CMainWindow()
 
 	delete(diaporamaTimer);
 
+	CMainParam* config = CMainParamInit::getInstance();
+	if (config != nullptr)
+		config->SetLastShowPicture(localFilename);
+
 	//if (refreshTimer->IsRunning())
 	//	refreshTimer->Stop();
 
