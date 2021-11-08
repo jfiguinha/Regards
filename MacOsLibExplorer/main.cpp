298e97aecb2f224fb8ab3feef8ebd9a656939f59
Figuinha <jfiguinha@hotmail.fr>
2021-11-08 20:39:56 +0100

    Update mac project

--- MacOsLibExplorer/main.cpp
+++ MacOsLibExplorer/main.cpp
@@ -17,6 +17,26 @@ bool SaveArrayStringToFile(wxTextFile & txtFile,const wxArrayString &aryStr)
     return true; 
 } 
 
+std::vector<wxString> split(const wxString& s, char seperator)
+{
+	std::vector<wxString> output;
+
+	wxString::size_type prev_pos = 0, pos = 0;
+
+	while ((pos = s.find(seperator, pos)) != std::string::npos)
+	{
+		wxString substring(s.substr(prev_pos, pos - prev_pos));
+
+		output.push_back(substring);
+
+		prev_pos = ++pos;
+	}
+
+	output.push_back(s.substr(prev_pos, pos - prev_pos)); // Last word
+
+	return output;
+}
+
 int main( int argc, char** argv )
 {
 	// initialize wxWidgets
@@ -36,14 +56,18 @@ int main( int argc, char** argv )
         printf("File : %s\n", files[i].ToStdString().c_str());
         wxString command = "otool -L " + files[i];
         wxExecute(command.c_str(), output);
+        
+        std::vector<wxString> libName = split(files[i], '/');
+        wxString dylibName = libName[libName.size() - 1];
         //SaveArrayStringToFile(txtFile, output);
-        for(int i=2;i<output.GetCount();i++) 
+        for(int i=1;i<output.GetCount();i++) 
         { 
             wxString lineText = output.Item(i);
             //int index = lineText.find("/System/Library/Frameworks");
              //printf("index %i \n", index);
             
-            if(!(lineText.find("/System/Library/Frameworks") == 1 || lineText.find("/usr/lib") == 1 || lineText.find("@executable_path") == 1))
+            
+            if(!(lineText.find("/System/Library/Frameworks") == 1 || lineText.find(dylibName) > 1 || lineText.find("/usr/lib") == 1 || lineText.find("@executable_path") == 1))
             {
                 printf("Line %s\n", lineText.ToStdString().c_str());
                 txtFile.AddLine(lineText);                
