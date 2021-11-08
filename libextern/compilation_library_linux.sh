9bcd21c40af689d75d600e079ae026b8e9c2c6d0
jfiguinha <jfiguinha@hotmail.fr>
2021-11-08 14:23:34 +0100

    Update compilation

--- libextern/compilation_library_linux.sh
+++ libextern/compilation_library_linux.sh
@@ -13,34 +13,8 @@ make -j$NBPROC
 cd ..
 cd ..
 
-
-FILE=opencv/opencv_contrib-4.5.1.zip
-if [ ! -f FILE ]; then
-    mkdir opencv
-
-    wget https://github.com/opencv/opencv_contrib/archive/4.5.1.zip
-    mv 4.5.1.zip opencv/opencv_contrib-4.5.1.zip
-
-    wget https://github.com/opencv/opencv/archive/4.5.1.zip
-    mv 4.5.1.zip opencv/opencv-4.5.1.zip
-fi
-
-#compile opencv
-cd opencv
-unzip opencv-4.5.1.zip
-unzip opencv_contrib-4.5.1.zip
-cd opencv-4.5.1
-mkdir build
-cd build
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DCMAKE_INSTALL_PREFIX:PATH="$HOME/ffmpeg_build" -DBUILD_opencv_python=OFF -DOPENCV_EXTRA_MODULES_PATH:PATH="../../opencv_contrib-4.5.1/modules" -DBUILD_opencv_freetype=OFF -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_apps=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_CXX_FLAGS="-std=gnu++14 -I ../../../eigen/Eigen" -DOPENCV_ALLOCATOR_STATS_COUNTER_TYPE=int64_t ../
-make -j$NBPROC
-sudo make install
-cd ..
-cd ..
-cd ..
-
 #Compile qpdf
-FILE=4.5.1.zip
+FILE=release-qpdf-10.3.2.zip
 if [ ! -f FILE ]; then
     wget https://github.com/qpdf/qpdf/archive/refs/tags/release-qpdf-10.3.2.zip
     unzip release-qpdf-10.3.2.zip
@@ -79,6 +53,7 @@ cd vcpkg-master
 ./vcpkg install sqlite3
 ./vcpkg install freeimage
 ./vcpkg install sdl2
+./vcpkg install opencv[contrib]
 cd ..
 
 unzip rav1e-0.5.0-beta.2-ubuntu.zip
