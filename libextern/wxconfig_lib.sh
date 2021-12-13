65021d937fea4a41d084a2f64b6050f1340bbd62
Figuinha <jfiguinha@hotmail.fr>
2021-12-13 15:45:32 +0100

    Update project for mac intel

--- libextern/wxconfig_lib.sh
+++ libextern/wxconfig_lib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $OSTYPE == 'darwin'* ]]; then
-	/opt/homebrew/bin/wx-config --libs
+	/usr/local/bin/wx-config --libs
 	#${HOME}/developpement/git/Regards/libextern/vcpkg-master/installed/arm64-osx/bin/wx-config --libs
 else
 #	${HOME}/ffmpeg_build/bin/wx-config --libs
