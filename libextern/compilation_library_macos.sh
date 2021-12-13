8f21efd7edc175dd9330978df2dd464f2fa19430
Figuinha <jfiguinha@hotmail.fr>
2021-12-13 13:42:11 +0100

    update lib compilation

--- libextern/compilation_library_macos.sh
+++ libextern/compilation_library_macos.sh
@@ -13,4 +13,13 @@ make -j$NBPROC
 cd ..
 cd ..
 
+git clone https://github.com/oneapi-src/oneTBB.git
+cd oneTBB
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX:PATH="$HOME/ffmpeg_build" -DBUILD_SHARED_LIBS=ON -DTBB_TEST=FALSE
+make -j$NBPROC
+make install
+cd ..
+cd ..
 
