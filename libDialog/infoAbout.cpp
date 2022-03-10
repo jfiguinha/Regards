c0f98f6ce61c78508cc169072473a5feb0c56276
Figuinha <jfiguinha@hotmail.fr>
2022-03-10 11:42:29 +0100

    Update version

--- libDialog/infoAbout.cpp
+++ libDialog/infoAbout.cpp
@@ -48,7 +48,7 @@ void CInfoAbout::Init()
 	auto treeDataWidth = new CTreeData();
 	treeDataWidth->SetIsParent(false);
 	treeDataWidth->SetKey("Version");
-	treeDataWidth->SetValue("2.70.0");
+	treeDataWidth->SetValue("2.70.1");
 	tr.append_child(child, treeDataWidth);
 
 	auto treeDataAnnee = new CTreeData();
