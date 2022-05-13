dacf5b1a5d8e3640aa608c1375260e22dbdea100
Figuinha <jfiguinha@hotmail.fr>
2022-05-13 17:45:15 +0200

    Correct bug on zoom at 100

--- libDialog/infoAbout.cpp
+++ libDialog/infoAbout.cpp
@@ -48,7 +48,7 @@ void CInfoAbout::Init()
 	auto treeDataWidth = new CTreeData();
 	treeDataWidth->SetIsParent(false);
 	treeDataWidth->SetKey("Version");
-	treeDataWidth->SetValue("2.71.5");
+	treeDataWidth->SetValue("2.71.6");
 	tr.append_child(child, treeDataWidth);
 
 	auto treeDataAnnee = new CTreeData();
