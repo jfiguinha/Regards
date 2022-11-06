d30079a1ff6b4f197bc3c6404152269b65c1405c
Figuinha <jfiguinha@hotmail.fr>
2022-11-06 20:29:24 +0100

    Cleaning code

---
+++ libextern/vcpkg_arm.bat
@@ -0,0 +1,22 @@
+vcpkg install libde265:arm64-windows --recurse
+vcpkg install sdl2:arm64-windows --recurse
+vcpkg install sqlite3:arm64-windows --recurse
+vcpkg install freeglut:arm64-windows --recurse
+vcpkg install tesseract:arm64-windows --recurse
+vcpkg install x265:arm64-windows --recurse
+vcpkg install OpenExr:arm64-windows --recurse
+vcpkg install libraw:arm64-windows --recurse
+vcpkg install jasper:arm64-windows --recurse
+vcpkg install exiv2[video,xmp]:arm64-windows --recurse
+vcpkg install dav1d:arm64-windows --recurse
+vcpkg install wxWidgets:arm64-windows --recurse
+vcpkg install opencv[contrib,ipp,openmp,opengl,ffmpeg]:arm64-windows --recurse
+vcpkg install freeimage:arm64-windows --recurse
+vcpkg install aom:arm64-windows --recurse
+vcpkg install poppler:arm64-windows --recurse
+vcpkg install libmediainfo:arm64-windows --recurse
+vcpkg install libepoxy:arm64-windows --recurse
+vcpkg install opencl:arm64-windows --recurse
+vcpkg install libexif:arm64-windows
+vcpkg install libjxl:arm64-windows
+vcpkg install rapidxml:arm64-windows
