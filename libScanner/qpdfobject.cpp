93a1787638adc786c8c77bc1c0b8ca05389f4864
Figuinha <jfiguinha@hotmail.fr>
2022-11-17 13:26:23 +0100

    Update qpdf

--- libScanner/qpdfobject.cpp
+++
@@ -1,9 +0,0 @@
-#include <header.h>
-#include <qpdf/QPDFObject.hh>
-#ifndef wxMSVC_VERSION
-void QPDFObject::setDescription(QPDF* qpdf, std::string const& description)
-{
-    this->owning_qpdf = qpdf;
-    this->object_description = description;
-}
-#endif
