7cf743912744e8d4d61a022804c875e934dae380
Figuinha <jfiguinha@hotmail.fr>
2022-12-10 20:14:11 +0100

    Update code for ARM

--- Otherlib/libheif/box_fuzzer.cc
+++ Otherlib/libheif/box_fuzzer.cc
@@ -23,24 +23,3 @@
 #include "box.h"
 #include "bitstream.h"
 #include "logging.h"
-
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
-{
-  auto reader = std::make_shared<heif::StreamReader_memory>(data, size, false);
-
-  heif::BitstreamRange range(reader, size);
-  for (;;) {
-    std::shared_ptr<heif::Box> box;
-    heif::Error error = heif::Box::read(range, &box);
-    if (error != heif::Error::Ok || range.error()) {
-      break;
-    }
-
-    box->get_type();
-    box->get_type_string();
-    heif::Indent indent;
-    box->dump(indent);
-  }
-
-  return 0;
-}
