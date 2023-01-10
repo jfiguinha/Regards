c550d08d9c572ea9eccd0be6424dbab7bb5a5ce3
Figuinha <jfiguinha@hotmail.fr>
2023-01-10 12:35:36 +0100

    Update version to 2.74.8

--- libDialog/infoAbout.cpp
+++ libDialog/infoAbout.cpp
@@ -57,7 +57,7 @@ void CInfoAbout::Init()
 	auto treeDataAnnee = new CTreeData();
 	treeDataAnnee->SetIsParent(false);
 	treeDataAnnee->SetKey("Copyright");
-	treeDataAnnee->SetValue("2014 - 2022");
+	treeDataAnnee->SetValue("2014 - 2023");
 	tr.append_child(child, treeDataAnnee);
 
 	auto treeDataLib = new CTreeData();
