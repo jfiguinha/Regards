0d4bc8c3d25ade10bf33c1b3d3ab23b2c6b2de98
Figuinha <jfiguinha@hotmail.fr>
2023-01-13 11:23:30 +0100

    Update libraw package

--- libextern/rawspeed-3.0/cmake/Modules/thread-local.cmake
+++ libextern/rawspeed-3.0/cmake/Modules/thread-local.cmake
@@ -4,7 +4,6 @@ CHECK_CXX_SOURCE_COMPILES("
 static thread_local int tls;
 int main(void)
 {
-  (void)tls;
   return 0;
 }" HAVE_CXX_THREAD_LOCAL)
 if(HAVE_CXX_THREAD_LOCAL)
@@ -15,7 +14,6 @@ CHECK_CXX_SOURCE_COMPILES("
 static __thread int tls;
 int main(void)
 {
-  (void)tls;
   return 0;
 }" HAVE_GCC_THREAD_LOCAL)
 if(HAVE_GCC_THREAD_LOCAL)
