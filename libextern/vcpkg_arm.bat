70bfdcb63c5c28345f993e17b397ca1145793aa5
Figuinha <jfiguinha@hotmail.fr>
2023-04-13 18:50:21 +0200

    Update projet for debug

--- libextern/vcpkg_arm.bat
+++ libextern/vcpkg_arm.bat
@@ -1,17 +1,17 @@
-vcpkg install libde265:arm64-windows --recurse
+vcpkg install libavif:arm64-windows
+vcpkg install libheif:arm64-windows
 vcpkg install sdl2:arm64-windows --recurse
 vcpkg install sqlite3:arm64-windows --recurse
+vcpkg install freeimage:arm64-windows --recurse
 vcpkg install freeglut:arm64-windows --recurse
 vcpkg install tesseract:arm64-windows --recurse
-vcpkg install x265:arm64-windows --recurse
-vcpkg install OpenExr:arm64-windows --recurse
+vcpkg install openexr:arm64-windows --recurse
 vcpkg install libraw:arm64-windows --recurse
 vcpkg install jasper:arm64-windows --recurse
+vcpkg install wxwidgets[webview]:arm64-windows --recurse
 vcpkg install exiv2[video,xmp]:arm64-windows --recurse
-vcpkg install dav1d:arm64-windows --recurse
+vcpkg install ffmpeg[gpl,aom,x264,x265,vpx,webp,vorbis,mp3lame,opencl,openjpeg,opus]:arm64-windows
 vcpkg install opencv[contrib,ipp,openmp,opengl,ffmpeg]:arm64-windows --recurse
-vcpkg install freeimage:arm64-windows --recurse
-vcpkg install aom:arm64-windows --recurse
 vcpkg install poppler:arm64-windows --recurse
 vcpkg install libmediainfo:arm64-windows --recurse
 vcpkg install libepoxy:arm64-windows --recurse
