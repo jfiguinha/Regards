c2d619b2a39cf269ebe6bfa5deb029a6bf2ae4c9
Figuinha <jfiguinha@hotmail.fr>
2023-09-11 15:29:02 +0200

    Update version

--- libextern/wxconfig_cpp.sh
+++ libextern/wxconfig_cpp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [[ $OSTYPE == 'darwin'* ]]; then
-	/usr/local/bin/wx-config --cppflags
+	/opt/homebrew/bin/wx-config --cppflags
 	#${HOME}/developpement/git/Regards/libextern/vcpkg-master/installed/arm64-osx/bin/wx-config --cppflags
 	#/Users/jacques/developpement/git/Regards/libextern/vcpkg-master/installed/arm64-osx/tools/wxwidgets/wx-config --cppflags
 else
 #	${HOME}/ffmpeg_build/bin/wx-config --cppflags
-    ${HOME}/developpement/git/Regards/libextern/vcpkg-2022.08.15/installed/x64-linux/tools/wxwidgets/wx-config --cppflags
+    ${HOME}/developpement/git/Regards/libextern/vcpkg-master/installed/x64-linux/tools/wxwidgets/wx-config --cppflags
 fi
 
 
