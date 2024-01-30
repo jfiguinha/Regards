bc403d66ff8b0f16c35ec1f42a5d54b9c7cc0891
Figuinha <jfiguinha@hotmail.fr>
2024-01-30 14:57:06 +0100

    Update compilation

--- libextern/compilation_library_linux.sh
+++ libextern/compilation_library_linux.sh
@@ -44,6 +44,16 @@ cd ..
 unzip ffmpeg-master_linux.zip
 mv ffmpeg-master_linux ffmpeg-master
 
+patchelf --force-rpath --set-rpath '$ORIGIN' ./ffmpeg-master/lib/libavcodec.so
+patchelf --force-rpath --set-rpath '$ORIGIN' ./ffmpeg-master/lib/libavdevice.so
+patchelf --force-rpath --set-rpath '$ORIGIN' ./ffmpeg-master/lib/libavfilter.so
+patchelf --force-rpath --set-rpath '$ORIGIN' ./ffmpeg-master/lib/libavformat.so
+patchelf --force-rpath --set-rpath '$ORIGIN' ./ffmpeg-master/lib/libavutil.so
+patchelf --force-rpath --set-rpath '$ORIGIN' ./ffmpeg-master/lib/libpostproc.so
+patchelf --force-rpath --set-rpath '$ORIGIN' ./ffmpeg-master/lib/libswresample.so
+patchelf --force-rpath --set-rpath '$ORIGIN' ./ffmpeg-master/lib/libswscale.so
+
+
 #Compile qpdf
 FILE=release-qpdf-10.3.2.zip
 if [ ! -f FILE ]; then
