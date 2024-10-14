dd1596732e080fb783aa2db4277ca7fa13ce8f47
Figuinha <jfiguinha@hotmail.fr>
2024-10-14 13:55:45 +0200

    Update project with dynamic lib vcpkg

--- RegardsViewer_linux/patch_release.sh
+++ RegardsViewer_linux/patch_release.sh
@@ -1,1 +1,4 @@
 patchelf --force-rpath --set-rpath '/opt/RegardsViewer' ./Release/RegardsViewer 
+patchelf --force-rpath --set-rpath '/opt/RegardsViewer' ./Release/*.so.* 
+#patchelf --force-rpath --set-rpath '.' ./Release/RegardsViewer 
+#patchelf --force-rpath --set-rpath '.' ./Release/*.so.* 
