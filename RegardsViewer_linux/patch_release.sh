fc0e341d3b79f88970628f353c05109a758a07a1
Figuinha <jfiguinha@hotmail.fr>
2024-10-18 10:58:13 +0200

    Update project

--- RegardsViewer_linux/patch_release.sh
+++ RegardsViewer_linux/patch_release.sh
@@ -1,5 +1,5 @@
-patchelf --force-rpath --set-rpath '/opt/RegardsViewer' ./Release/RegardsViewer 
-patchelf --force-rpath --set-rpath '/opt/RegardsViewer' ./Release/ffmpeg 
-patchelf --force-rpath --set-rpath '/opt/RegardsViewer' ./Release/*.so.* 
+patchelf --force-rpath --set-rpath '$ORIGIN' ./Release/RegardsViewer 
+patchelf --force-rpath --set-rpath '$ORIGIN' ./Release/ffmpeg 
+patchelf --force-rpath --set-rpath '$ORIGIN' ./Release/*.so.* 
 #patchelf --force-rpath --set-rpath '.' ./Release/RegardsViewer 
 #patchelf --force-rpath --set-rpath '.' ./Release/*.so.* 
