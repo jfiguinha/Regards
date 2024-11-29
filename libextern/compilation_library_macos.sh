a3a5ff5bea24c45d1203f84aacbc5b6178200fbd
Figuinha <jfiguinha@hotmail.fr>
2024-11-29 15:00:54 +0100

    Update project for mac

--- libextern/compilation_library_macos.sh
+++ libextern/compilation_library_macos.sh
@@ -15,8 +15,12 @@ cd vcpkg
 ./bootstrap-vcpkg.sh
 ./vcpkg install wxwidgets[webview]
 ./vcpkg install sdl2
-
 cd ..
+
+unzip ports_vcpkg_mac.zip
+
+cp -r ports_vcpkg/opencv4 vcpkg/ports
+
 unzip SVT-AV1-v2.2.1.zip
 cd SVT-AV1-v2.2.1/Build
 PATH="$HOME/bin:$PATH" cmake -DCMAKE_INSTALL_PREFIX="$LOCALPATH/vcpkg/installed/arm64-osx" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF ..
@@ -30,7 +34,7 @@ cd ..
 
 cd vcpkg
 ./vcpkg install ffmpeg[gpl,aom,dav1d,x265,x264,openh264,vpx,webp,vorbis,mp3lame,openjpeg,opus,fdk-aac,ffmpeg] --allow-unsupported
-./vcpkg install opencv4[contrib,core,dnn,ffmpeg,ipp,jpeg,tbb,png,tiff,webp,openexr]
+./vcpkg install opencv4[contrib,core,dnn,ffmpeg,ipp,jpeg,tbb,png,tiff,webp,openexr,opengl]
 ./vcpkg install libde265
 ./vcpkg install exiv2[video,xmp,bmff]
 ./vcpkg install libavif[aom,dav1d]
