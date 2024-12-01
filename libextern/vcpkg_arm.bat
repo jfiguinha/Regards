148ce114ca424d9a307c70a4b64daca40581f58b
Jacques Figuinha <jfiguinha@hotmail.fr>
2024-12-01 19:46:13 +0100

    Update for windows on arm

--- libextern/vcpkg_arm.bat
+++ libextern/vcpkg_arm.bat
@@ -10,8 +10,8 @@ vcpkg install libraw:arm64-windows --recurse
 vcpkg install jasper:arm64-windows --recurse
 vcpkg install wxwidgets[webview]:arm64-windows --recurse
 vcpkg install exiv2[video,xmp]:arm64-windows --recurse
-vcpkg install ffmpeg[gpl,aom,dav1d,x265,x264,openh264,vpx,webp,vorbis,mp3lame,opencl,openjpeg,opus,fdk-aac,ffmpeg]:arm64-windows --recurse
-vcpkg install opencv[contrib,ipp,openmp,opengl,ffmpeg]:arm64-windows --recurse
+vcpkg install ffmpeg[gpl,aom,dav1d,x265,x264,openh264,vpx,webp,vorbis,mp3lame,opencl,openjpeg,opus,fdk-aac,ffmpeg,opengl]:arm64-windows --recurse
+vcpkg install opencv4[contrib,core,dnn,ffmpeg,ipp,jpeg,tbb,png,tiff,webp,openexr,opengl]:arm64-windows --recurse
 vcpkg install poppler:arm64-windows --recurse
 vcpkg install libmediainfo:arm64-windows --recurse
 vcpkg install libepoxy:arm64-windows --recurse
@@ -19,3 +19,5 @@ vcpkg install opencl:arm64-windows --recurse
 vcpkg install libexif:arm64-windows
 vcpkg install libjxl:arm64-windows
 vcpkg install rapidxml:arm64-windows
+vcpkg install openal-soft:arm64-windows
+vcpkg install ncnn[vulkan]:arm64-windows --allow-unsupported
