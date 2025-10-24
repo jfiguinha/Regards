be2e0e9a0d88c9edd338e95818ccaf9adae5d2b4
jfiguinha <jfiguinha@hotmail.fr>
2025-10-24 19:49:18 +0200

    Update linux project

--- libextern/compilation_library_linux.sh
+++ libextern/compilation_library_linux.sh
@@ -5,52 +5,17 @@ echo $NBPROC
 LOCALPATH=$(pwd)
 echo $LOCALPATH
 
-
-
 export PKG_CONFIG_PATH=$HOME/ffmpeg_build/lib/pkgconfig
 
-FILE=/usr/local/cuda/version.json
-if [ -f "$FILE" ]; 
-then
-    echo "Cuda found in this system"
-    export CC=/usr/bin/gcc-10
-    export CXX=/usr/bin/g++-10
-    export CUDA_ROOT=/usr/local/cuda
-    sudo rm $CUDA_ROOT/bin/gcc
-    sudo rm $CUDA_ROOT/bin/g++
-    sudo ln -s /usr/bin/gcc-10 $CUDA_ROOT/bin/gcc
-    sudo ln -s /usr/bin/g++-10 $CUDA_ROOT/bin/g++
-    cp header_cuda.h ../include/header.h
-else
-    echo "Cuda not found in system"
-    cp header.h ../include/header.h
-fi
-
-unzip vcpkg-2024.08.23_linux.zip
-mv vcpkg-2024.08.23 vcpkg
-
-unzip ports_vcpkg.zip
-
-x11=$XDG_SESSION_TYPE
-echo $x11
-
-if [ "$x11" = "x11" ]
-then
-    cp ports_vcpkg/x11/wxwidgets/portfile.cmake vcpkg/ports/wxwidgets/portfile.cmake 
-fi
-
-cp -r ports_vcpkg/opencv4 vcpkg/ports
+unzip vcpkg-2025.09.17_linux.zip
+mv vcpkg-2025.09.17 vcpkg
 
 cd vcpkg
 ./bootstrap-vcpkg.sh
-./vcpkg install wxwidgets
+./vcpkg install wxwidgets[webview]
 ./vcpkg install x265
 ./vcpkg install libde265
-if [ -f "$FILE" ]; then
-    ./vcpkg install opencv4[contrib,core,dnn,dnn-cuda,ffmpeg,ipp,jpeg,openmp,png,tiff,webp,openexr,opengl,cuda,gtk]
-else
-    ./vcpkg install opencv4[contrib,core,dnn,ffmpeg,ipp,jpeg,openmp,png,tiff,webp,openexr,opengl,gtk]
-fi
+./vcpkg install opencv4[contrib,core,dnn,ffmpeg,ipp,jpeg,openmp,png,tiff,webp,openexr,opengl,opencl,gtk]
 ./vcpkg install opencl
 ./vcpkg install tbb
 ./vcpkg install exiv2[video,xmp,bmff]
