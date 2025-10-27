12d7002ba23ca0a097db1ba15b993e4e0de60574
Jacques Figuinha <jfiguinha@hotmail.fr>
2025-10-27 17:42:46 +0100

    Update arm win64 project

--- libextern/vcpkg_arm.bat
+++ libextern/vcpkg_arm.bat
@@ -1,23 +1,23 @@
-vcpkg install libavif[aom]:arm64-windows
-vcpkg install libheif:arm64-windows
-vcpkg install sdl2:arm64-windows --recurse
-vcpkg install sqlite3:arm64-windows --recurse
-vcpkg install freeimage:arm64-windows --recurse
-vcpkg install freeglut:arm64-windows --recurse
-vcpkg install tesseract:arm64-windows --recurse
-vcpkg install openexr:arm64-windows --recurse
-vcpkg install libraw:arm64-windows --recurse
-vcpkg install jasper:arm64-windows --recurse
-vcpkg install wxwidgets[webview]:arm64-windows --recurse
-vcpkg install exiv2[video,xmp]:arm64-windows --recurse
-vcpkg install ffmpeg[gpl,aom,dav1d,x265,x264,openh264,vpx,webp,vorbis,mp3lame,opencl,openjpeg,opus,fdk-aac,ffmpeg,opengl]:arm64-windows --recurse
-vcpkg install opencv4[contrib,core,dnn,ffmpeg,ipp,jpeg,tbb,png,tiff,webp,openexr,opengl]:arm64-windows --recurse
-vcpkg install poppler:arm64-windows --recurse
-vcpkg install libmediainfo:arm64-windows --recurse
-vcpkg install libepoxy:arm64-windows --recurse
-vcpkg install opencl:arm64-windows --recurse
-vcpkg install libexif:arm64-windows
-vcpkg install libjxl:arm64-windows
-vcpkg install rapidxml:arm64-windows
-vcpkg install openal-soft:arm64-windows
 vcpkg install ncnn[vulkan]:arm64-windows --allow-unsupported
+vcpkg install wxWidgets[webview]:arm64-windows
+vcpkg install sdl2:arm64-windows
+vcpkg install x265:arm64-windows
+vcpkg install libde265:arm64-windows
+vcpkg install tesseract:arm64-windows --allow-unsupported
+vcpkg install opencv4[contrib,core,dnn,ffmpeg,jpeg,tbb,png,tiff,webp,openexr,opengl]:arm64-windows --recurse --allow-unsupported
+vcpkg install opencl:arm64-windows
+vcpkg install dav1d:arm64-windows
+vcpkg install exiv2[video,xmp,bmff]:arm64-windows
+vcpkg install libavif[aom,dav1d]:arm64-windows
+vcpkg install libmediainfo:arm64-windows
+vcpkg install libexif:arm64-windows
+vcpkg install jasper:arm64-windows
+vcpkg install libraw[dng-lossy,openmp]:arm64-windows
+vcpkg install poppler:arm64-windows
+vcpkg install sqlite3:arm64-windows
+vcpkg install freeimage:arm64-windows
+vcpkg install libjxl:arm64-windows
+vcpkg install libepoxy:arm64-windows
+vcpkg install boost-lexical-cast:arm64-windows
+vcpkg install freeglut:arm64-windows
+vcpkg install libxml2:arm64-windows
