911cfb8cb2d7ecfc4a434d11004028899130fdc4
jfiguinha <jfiguinha@hotmail.fr>
2026-02-19 09:12:40 +0100

    Add webview project to prevent crash on snap

---
+++ webview/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <map>
+#include <iostream>
+#include <wx/webview.h>
+using namespace std;
+
+
+int main( int argc, char** argv )
+{
+    wxWebView * webBrowser = nullptr;
+    webBrowser = wxWebView::New();
+    printf("99\n");
+	return 99;
+}
