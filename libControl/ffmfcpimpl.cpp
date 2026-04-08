02c9b85df04fca4a8a16640375ef6fe30427a64c
Jacques Figuinha <jfiguinha@hotmail.fr>
2026-04-08 19:44:44 +0200

    Update

--- libControl/ffmfcpimpl.cpp
+++ libControl/ffmfcpimpl.cpp
@@ -443,12 +443,12 @@ void CFFmfcPimpl::video_display(VideoState* is)
 						&convertedFrameBuffer, &linesize);
 				}
 			}
-			dlg->SetData(dataFrame);
-			/*
+			//dlg->SetData(dataFrame);
+			
 			wxCommandEvent event(wxEVENT_UPDATEFRAME);
 			event.SetClientData(dataFrame);
 			wxPostEvent(dlg->GetMainWindow(), event);
-			*/
+			
 		}
 
 		//}
